7999ec5dccd26b07af75ab817a32d75d9805f529
Mahmud Wunnam <38923023+MahmudSuleman@users.noreply.github.com>
2025-06-17 17:57:58 +0000

    wip

--- app/Console/Commands/TestMail.php
+++ app/Console/Commands/TestMail.php
@@ -9,8 +9,7 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Mail;
 use Symfony\Component\Mime\Email;
 use Illuminate\Mail\Mailable;
-class TestMail extends Command implements ShouldQueue
-{
+class TestMail extends Command{
     /**
      * The name and signature of the console command.
      *
