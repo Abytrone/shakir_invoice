695d6f79ffed1c88fda4f9fb8ce217068e0c7957
Mahmud Wunnam <38923023+MahmudSuleman@users.noreply.github.com>
2025-09-15 07:58:43 +0000

    wip

--- routes/console.php
+++ routes/console.php
@@ -10,32 +10,38 @@ use App\Console\Commands\UpdateInvoiceStatus;
 
 Schedule::command(GenerateRecurringInvoices::class)
     ->dailyAt('00:00')
+    ->timeZone('Africa/Accra')
     ->appendOutputTo(storage_path('logs/recurring-invoices.log'));
 
 Schedule::command(UpdateInvoiceStatus::class)
     ->dailyAt('00:00')
+    ->timeZone('Africa/Accra')
     ->appendOutputTo(storage_path('logs/update-invoice-status.log'));
 
 Schedule::command(RecurringInvoiceReminder::class)
     ->dailyAt('00:00')
+    ->timeZone('Africa/Accra')
     ->appendOutputTo(storage_path('logs/recurring-invoice-reminder.log'));
 
 Schedule::command( UpdateInvoiceOverDueStatus::class)
     ->dailyAt('00:00')
+    ->timeZone('Africa/Accra')
     ->appendOutputTo(storage_path('logs/update-invoice-over-due-status.log'));
 
 
-Schedule::command('telescope:prune --hours=48')->dailyAt('00:00');
+Schedule::command('telescope:prune --hours=48')
+    ->dailyAt('00:00')
+    ->timeZone('Africa/Accra')
+    ->appendOutputTo(storage_path('logs/scheduler-is-still-running.log'));
 
 Schedule::command(SchedulerIsStillRunning::class)
-//    ->everyFiveSeconds()
     ->dailyAt(now()->addMinutes(5)->format('H:i'))
+    ->timeZone('Africa/Accra')
     ->appendOutputTo(storage_path('logs/scheduler-is-still-running.log'));
 
 
 Schedule::command(AutoBillClient::class)
     ->dailyAt('00:00')
+    ->timeZone('Africa/Accra')
     ->appendOutputTo(storage_path('logs/auto-bill-client.log'));
 
-//Schedule::command(LogToFile::class)
-//    ->everyMinute();
