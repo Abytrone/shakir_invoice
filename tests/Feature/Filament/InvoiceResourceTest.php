2db8bea75a4933b5e4f4b5ebf26311a9e82c1c71
Mahmud Wunnam <38923023+MahmudSuleman@users.noreply.github.com>
2026-04-10 16:11:51 +0000

    add some tests for invoice resource

--- tests/Feature/Filament/InvoiceResourceTest.php
+++ tests/Feature/Filament/InvoiceResourceTest.php
@@ -1,10 +1,13 @@
 <?php
 
-namespace Tests\Feature\Filament;
+namespace Filament;
 
 use App\Filament\Resources\InvoiceResource;
+use App\Filament\Resources\InvoiceResource\Pages\ListInvoices;
 use App\Models\Invoice;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Livewire\Livewire;
 use Tests\Feature\Filament\Concerns\ActsAsAdmin;
 use Tests\TestCase;
 
@@ -50,5 +53,42 @@ class InvoiceResourceTest extends TestCase
         $this->actingAsAdmin()
             ->get(InvoiceResource::getUrl('edit', ['record' => $invoice]))
             ->assertOk();
+    }
+
+    public function test_list_page_hides_create_button_without_create_invoice_permission(): void
+    {
+        $user = User::factory()->create();
+        $user->assignRole('user');
+
+        $this->actingAs($user);
+
+        Livewire::test(ListInvoices::class)
+            ->assertActionHidden('create');
+    }
+
+    public function test_list_page_hides_edit_action_without_update_invoice_permission(): void
+    {
+        $invoice = Invoice::factory()->create();
+
+        $user = User::factory()->create();
+        $user->assignRole('user');
+
+        $this->actingAs($user);
+
+        Livewire::test(ListInvoices::class)
+            ->assertTableActionHidden('edit', $invoice);
+    }
+
+    public function test_list_page_hides_delete_action_without_delete_invoice_permission(): void
+    {
+        $invoice = Invoice::factory()->create();
+
+        $user = User::factory()->create();
+        $user->assignRole('user');
+
+        $this->actingAs($user);
+
+        Livewire::test(ListInvoices::class)
+            ->assertTableActionHidden('delete', $invoice);
     }
 }
